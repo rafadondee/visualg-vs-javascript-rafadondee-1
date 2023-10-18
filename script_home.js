a6ed49c1f4876518f98a65821d1e03e0a9e01435
rafadondee <127406148+rafadondee@users.noreply.github.com>
2023-10-18 11:01:44 -0300

    Delete script_home.js

--- script_home.js
+++
@@ -1,5 +0,0 @@
-let usuario = JSON.parse(localStorage.getItem("logado"));
-document.getElementById("titulo").innerHTML = "Bem vindo, "+ usuario.login
-function voltar(){
-    window.location.href="../index.html"
-}
